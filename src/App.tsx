5edd351ea10891633a80561d071cc0414589946d
WepILoK <artur.nasyrov1999@mail.ru>
2021-08-15 22:51:39 +0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.scss';
+import {Header} from "./components/Header/Header";
 
 
 export const App = () => {
     return (
         <div className="App">
-
+            <Header/>
         </div>
     );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './Header.scss'
 
 export const Header = () => {
+    const [visibleUser, setVisibleUser] = useState(false)
+
+
     return (
         <div className='header'>
             <div className='header__inner'>
                 <div className="header__logo">
                     <img src={require("../../assets/images/anim-logo.png").default} alt='AnimLogo'/>
                 </div>
-                <div className="header__menu">
-
+                <div className="header__menu menu">
+                    <div className='menu__item'>Каталог</div>
+                    <div className='menu__item'>Поиск</div>
                 </div>
-                <div className="header__user">
-
+                <div className="header__user user">
+                    <div className='user__notifications'>
+                        <img src={require("../../assets/images/notifications.svg").default} alt='notifications'/>
+                    </div>
+                    <div className='user__name'>
+                        NAme
+                    </div>
+                    <div className='user__avatar'>
+                        <img src={require("../../assets/images/avatar.png").default} alt='Avatar'/>
+                    </div>
                 </div>
             </div>
         </div>
