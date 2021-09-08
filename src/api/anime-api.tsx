8970a2cee182f351d129ae1bfb6f4704277d6952
WepILoK <artur.nasyrov1999@mail.ru>
2021-09-08 21:36:26 +0300

    Правки

--- src/api/anime-api.tsx
+++ src/api/anime-api.tsx
@@ -2,19 +2,19 @@ import axios from "axios";
 
 export const AnimeApi = {
     async fetchAnimeList() {
-        const {data} = await axios.get('http://localhost:3001/animeList')
+        const {data} = await axios.get('/animeList')
         return data
     },
     async fetchNewEpisodesList() {
-        const {data} = await axios.get('http://localhost:3001/newEpisodes')
+        const {data} = await axios.get('/newEpisodes')
         return data
     },
     async fetchPopularList() {
-        const {data} = await axios.get('http://localhost:3001/popularList')
+        const {data} = await axios.get('/popularList')
         return data
     },
     async fetchTopAnimeList() {
-        const {data} = await axios.get('http://localhost:3001/animeList?_sort=views&_order=desc&_limit=10')
+        const {data} = await axios.get('/animeList?_sort=views&_order=desc&_limit=10')
         return data
     },
 }
